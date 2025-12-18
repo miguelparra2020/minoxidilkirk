b12cff9a40bb7b5911b48a4ab450cd0b58a93d92
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2025-12-17 20:35:54 -0500

    add-changes

--- utils/generalConfig.ts
+++ utils/generalConfig.ts
@@ -34,11 +34,11 @@ export const pathsIconos = {
   path2photos: "M14.002 13a2 2 0 0 1-2 2h-10a2 2 0 0 1-2-2V5A2 2 0 0 1 2 3a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v8a2 2 0 0 1-1.998 2M14 2H4a1 1 0 0 0-1 1h9.002a2 2 0 0 1 2 2v7A1 1 0 0 0 15 11V3a1 1 0 0 0-1-1M2.002 4a1 1 0 0 0-1 1v8l2.646-2.354a.5.5 0 0 1 .63-.062l2.66 1.773 3.71-3.71a.5.5 0 0 1 .577-.094l1.777 1.947V5a1 1 0 0 0-1-1z",
   }
 
-export const linkProducts = "https://www.google.com/";
-export const linkServices = "https://www.google.com/";
-export const linkPhotos = "https://www.google.com/";
+export const linkProducts = "https://wa.link/bwcdrb";
+export const linkServices = "https://wa.link/bwcdrb";
+export const linkPhotos = "https://wa.link/bwcdrb";
 export const LinkWhasapp = "https://wa.link/bwcdrb";
-export const LinkUsuario = "https://www.google.com/";
+export const LinkUsuario = "https://wa.link/bwcdrb";
 
             
 export const generalConfig = {
